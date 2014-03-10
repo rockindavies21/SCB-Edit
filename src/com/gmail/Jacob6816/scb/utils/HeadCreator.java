0c6d1c6c213c9d9ddfd5311f6f25e4a424033656
JRL1004 <Loosa.J.History@gmail.com>
2014-03-10 12:37:09 -0600

    1 File(s)

--- src/com/gmail/Jacob6816/scb/utils/HeadCreator.java
+++ src/com/gmail/Jacob6816/scb/utils/HeadCreator.java
@@ -3,6 +3,7 @@ package com.gmail.Jacob6816.scb.utils;
 import org.bukkit.Material;
 import org.bukkit.SkullType;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
 public class HeadCreator {
@@ -12,6 +13,14 @@ public class HeadCreator {
         ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.PLAYER.ordinal());
         SkullMeta meta = (SkullMeta) skull.getItemMeta();
         meta.setOwner(name);
+        meta.setDisplayName(display);
+        skull.setItemMeta(meta);
+        return skull;
+    }
+    
+    public ItemStack getMobhead(SkullType type, String display) {
+        ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) type.ordinal());
+        ItemMeta meta = skull.getItemMeta();
         meta.setDisplayName(display);
         skull.setItemMeta(meta);
         return skull;
