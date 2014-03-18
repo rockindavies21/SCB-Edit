5c5a37e0f713ff250f568b1d65f520e31ff88350
mikey1665 <gamermiek@gmail.com>
2014-03-17 19:37:27 -0500

    Lang

--- src/org/mcsg/double0negative/supercraftbros/Lang.java
+++ src/org/mcsg/double0negative/supercraftbros/Lang.java
@@ -9,13 +9,24 @@ import org.bukkit.configuration.file.YamlConfiguration;
 */
 public enum Lang {
     TITLE("title-name", "&4[&fSCB&4]:"),
-    PLAYER_JOIN("player-join", "&6%p Joined the game!"),
-    PLAYER_MSG_JOIN("player-msg-all-join", "%p Joined the game choose a class!"),
+    PLAYER_JOIN("player-join", "&2&lYOU &bJoined the game!"),
+    PLAYER_MSG_JOIN("player-msg-all-join", "%p Joined the game choose a class! &b[&2+&b]"),
+    SELF_LEAVE("self-leave", "You Left!"),
     PLAYER_LEAVE("player-leave", "&b&l%p Left!"),
     PLAYER_ELIMINATED("player-eliminated", "&4%p Lost the game!"),
     DIDNT_PICK_A_CLASS("no-class", "&cYou didnt pick a class."),
-    NO_PERMS("no-permissions", "&cYou don''t have permission for that!");
- 
+    NO_PERMS("no-permissions", "&cYou don''t have permission for that!"),
+    GAME_FULL("cannot-join-game-full", "&cYou don''t have permission for that!"),
+    GAME_STARTED("cannot-join-game-started", "&cYou don''t have permission for that!"),
+    GAME_CANNOT("cannot-join-game", "&cYou don''t have permission for that!"),
+    NO_CLASS("kicked-from-game-no-class", "&cYou don''t have permission for that!"),
+    COUNTDOWN("count-down", "&bGame starting in %c"),
+    CHOOSE_CLASS("class-chosen", "&6You chose %class"),
+    LIVES_LEFT("lives-left", "You have %lives Left!"),
+    PLAYER_ELIMINATE("player-eliminated", "%p Has been eliminated!"),
+    BROADCAST_WIN("player-win-broadcast", "%p Has won on %arena"),
+    NOT_ENOUGH("not-enough-players", "&cNOT ENOUGH PLAYERS");
+    
     private String path;
     private String def;
     private static YamlConfiguration LANG;
